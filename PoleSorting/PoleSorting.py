3c7248d9a2bbc1d19565a0d2d2f6a60ddd1bb951
pe0008 <parker.eddy@davey.com>
2025-01-12 16:37:44 -0800

    menu rework into classes

--- PoleSorting/PoleSorting.py
+++ PoleSorting/PoleSorting.py
@@ -1,151 +1,182 @@
 from Functions import get_and_process_folder, export_to_csv, validate_manual_sort, display_validation_results, read_pole_data
 from ImageFunctions import sort_into_folders, match_pole_to_trapezoid, extract_image_metadata, create_trapezoid
 
-
-def foldermenu():
-    print("Folder Based Analysis")
-    print("-" * 50)
-    print("1. Process Pole Folders")
-    print("2. View Results")
-    print("3. Export Results to CSV")
-    print("4. Validate Manual Sort")
-    print("5. Return to Main Menu")
-    print('-' * 50)
-
-def foldermain():
-    folder_matches = {}
-    while True:
-        foldermenu()
-        choice = input("Please select an option (1-5): ")
-
-        if choice == '1':
-            print('\nBeginning Pole Folder Processing.')
-            print('-' * 50)
-            #csv_file = input('Enter the path to the CSV file containing pole data: ')
-            csv_file = "D:/BearValleyTesting/polelocations.csv"
-            folder_matches = get_and_process_folder(csv_file)
-        elif choice == '2':
-            if folder_matches:
-                display_results(folder_matches)
-            else:
-                print("No results. Process pole folders first.")
-            returntomenu()
-        elif choice == '3':
-            if folder_matches:
-                csv_output_file = input('Enter the path to the CSV you wish to write to: ')
-                export_to_csv(folder_matches, csv_output_file)
-            else:
-                print("No results to export. Run option 1 first.")
-        elif choice == '4':
-            if folder_matches:
-                correct_matches, incorrect_folders = validate_manual_sort(folder_matches)
-                display_validation_results(correct_matches, incorrect_folders)
-            else:
-                print("Process pole folders first.")
-        elif choice == '5':
-            break
-        else:
-            print('Select a number from the list.')
-
-def imagemain():
-    while True:
-        imagemenu()
-        choice = input("Please select an option (1-4): ")
-
-        if choice == '1':
-            folder_path = input('Input folder path: ')
-            csv_file = input("Path to pole CSV file:")
-            #extract metadata
-            image_metadata = extract_image_metadata(folder_path)
-            #create approx viewshed trapezoids
-            trapezoids = create_trapezoid(image_metadata)
-            #get the pole locations
-            pole_data = read_pole_data(csv_file)
-            #see if any poles are in each viewshed
-            inside_poles = match_pole_to_trapezoid(trapezoids, pole_data)
-
-        elif choice == '2':
-            print('tbd')
-        elif choice == '3':
-            print('tbd')
-        elif choice == '4':
-            destination_folder = input('Input path where you want poles to be exported: ')
-            #if a picture has a matching pole, put it in the folder
-            sort_into_folders(inside_poles, destination_folder, folder_path)
-        elif choice == '5':
-            break
-        else:
-            print('Select a number from the list.')
-
-def imagemenu():
-    print("Image Based Analysis")
-    print("-" * 50)
-    print("1. Process Images")
-    print("2. View Results")
-    print("3. Export Results to CSV")
-    print("4. Export Matches to Pole Folders")
-    print("5. Return to Main Menu")
-    print('-' * 50)
-
 def returntomenu():
+    """Prompts the user to press Enter before returning to the menu."""
     input('\nPress Enter to return to the menu.')
 
-def display_results(folder_matches):
-    matched = []
-    no_gps_or_no_image = []
-    over_10m = []
+class FolderAnalysis:
 
-    #separate results into matches, no data, or bad matches
-    for folder, data in folder_matches.items():
-        if data['average_coordinates'] == (None, None):
-            no_gps_or_no_image.append(folder)
-        elif data['distance'] is not None and data ['distance'] * 111000 > 10:
-            over_10m.append(folder)
+    def __init__(self):
+        self.folder_matches = {}
+
+    def menu(self):
+        print("Folder Based Analysis")
+        print("-" * 50)
+        print("1. Process Pole Folders")
+        print("2. View Results")
+        print("3. Export Results to CSV")
+        print("4. Validate Manual Sort")
+        print("5. Return to Main Menu")
+        print('-' * 50)
+
+    def process_folders(self):
+        print('\nBeginning Pole Folder Processing.')
+        print('-' * 50)
+        csv_file = "D:/BearValleyTesting/polelocations.csv"
+        self.folder_matches = get_and_process_folder(csv_file)
+
+    def view_results(self):
+        if self.folder_matches:
+            self.display_results()
         else:
-            matched.append(folder)
+            print("No results. Process pole folders first.")
+        returntomenu()
 
-    print('\nFolders matched within 10 meters')
-    print('-' * 50)
-    for folder in matched:
-        matched_pole = folder_matches[folder]['closest_pole']
-        distance = folder_matches[folder]['distance'] * 111000
-        print(f"{folder}: Matched to {matched_pole}, Distance = {distance:.2f} m")
+    def export_results(self):
+        if self.folder_matches:
+            csv_output_file = input('Enter the path to the CSV you wish to write to: ')
+            export_to_csv(self.folder_matches, csv_output_file)
+        else:
+            print("No results to export. Run option 1 first.")
 
-    print("\nFolders matched outside of 10 meters")
-    print('-' * 50)
-    for folder in over_10m:
-        matched_pole = folder_matches[folder]['closest_pole']
-        avg_coords = folder_matches[folder]['average_coordinates']
-        dmsdistance = folder_matches[folder]['distance']
-        distance_in_meters = dmsdistance * 111000
-        print(f"{folder}: Closest Pole: {matched_pole} at a distance of {distance_in_meters:.2f} meters, Lat = {avg_coords[0]:.6f}°, Lon = {avg_coords[1]:.6f}°")
+    def validate_manual_sort(self):
+        if self.folder_matches:
+            correct_matches, incorrect_folders = validate_manual_sort(self.folder_matches)
+            display_validation_results(correct_matches, incorrect_folders)
+        else:
+            print("Process pole folders first.")
 
-    print("\nFolders with no GPS data or no images")
-    print('-' * 50)
-    for folder in no_gps_or_no_image:
+    def display_results(self):
+        matched, no_gps_or_no_image, over_10m = [], [], []
+
+        for folder, data in self.folder_matches.items():
+            if data['average_coordinates'] == (None, None):
+                no_gps_or_no_image.append(folder)
+            elif data['distance'] is not None and data['distance'] * 111000 > 10:
+                over_10m.append(folder)
+            else:
+                matched.append(folder)
+
+        print('\nFolders matched within 10 meters')
+        print('-' * 50)
+        for folder in matched:
+            matched_pole = self.folder_matches[folder]['closest_pole']
+            distance = self.folder_matches[folder]['distance'] * 111000
+            print(f"{folder}: Matched to {matched_pole}, Distance = {distance:.2f} m")
+
+        print("\nFolders matched outside of 10 meters")
+        print('-' * 50)
+        for folder in over_10m:
+            matched_pole = self.folder_matches[folder]['closest_pole']
+            avg_coords = self.folder_matches[folder]['average_coordinates']
+            distance_in_meters = self.folder_matches[folder]['distance'] * 111000
+            print(f"{folder}: Closest Pole: {matched_pole}, Distance = {distance_in_meters:.2f} meters, "
+                  f"Lat = {avg_coords[0]:.6f}°, Lon = {avg_coords[1]:.6f}°")
+
+        print("\nFolders with no GPS data or no images")
+        print('-' * 50)
+        for folder in no_gps_or_no_image:
             print(folder)
 
-def mainmenu():
-    print("Pole Sorting")
-    print("-" * 50)
-    print("1. Folder Based Analysis")
-    print("2. Image Based Analysis")
-    print("3. Exit Program")
-    print('-' * 50)
+    def run(self):
+        while True:
+            self.menu()
+            choice = input("Please select an option (1-5): ")
 
-def main():
-    while True:
-        mainmenu()
-        choice = input("Please select an option (1-3): ")
+            if choice == '1':
+                self.process_folders()
+            elif choice == '2':
+                self.view_results()
+            elif choice == '3':
+                self.export_results()
+            elif choice == '4':
+                self.validate_manual_sort()
+            elif choice == '5':
+                break
+            else:
+                print('Select a number from the list.')
 
-        if choice == '1':
-            foldermain()
-        elif choice == '2':
-            imagemain()
-        elif choice == '3':
-            break
+class ImageAnalysis:
+
+    def __init__(self):
+        self.inside_poles = None
+        self.folder_path = None
+
+    def menu(self):
+        print("Image Based Analysis")
+        print("-" * 50)
+        print("1. Process Images")
+        print("2. View Results")
+        print("3. Export Results to CSV")
+        print("4. Export Matches to Pole Folders")
+        print("5. Export to Shapefile")
+        print("6. Return to Main Menu")
+        print('-' * 50)
+
+    def process_images(self):
+        self.folder_path = input('Input folder path: ')
+        csv_file = input("Path to pole CSV file: ")
+
+        image_metadata = extract_image_metadata(self.folder_path)
+        trapezoids = create_trapezoid(image_metadata)
+        pole_data = read_pole_data(csv_file)
+        self.inside_poles = match_pole_to_trapezoid(trapezoids, pole_data)
+
+    def export_matches(self):
+        if self.inside_poles and self.folder_path:
+            destination_folder = input('Input path where you want poles to be exported: ')
+            sort_into_folders(self.inside_poles, destination_folder, self.folder_path)
         else:
-            print('Select a number from the list.')
+            print("No processed data. Run option 1 first.")
+
+    def run(self):
+        while True:
+            self.menu()
+            choice = input("Please select an option (1-5): ")
+
+            if choice == '1':
+                self.process_images()
+            elif choice == '2':
+                print('TBD')
+            elif choice == '3':
+                print('TBD')
+            elif choice == '4':
+                self.export_matches()
+            elif choice == '5':
+                print('tbd')
+            elif choice == '6':
+                break
+            else:
+                print('Select a number from the list.')
+
+class MainProgram:
+
+    def __init__(self):
+        self.folder_analysis = FolderAnalysis()
+        self.image_analysis = ImageAnalysis()
+
+    def menu(self):
+        print("Pole Sorting")
+        print("-" * 50)
+        print("1. Folder Based Analysis")
+        print("2. Image Based Analysis")
+        print("3. Exit Program")
+        print('-' * 50)
+
+    def run(self):
+        while True:
+            self.menu()
+            choice = input("Please select an option (1-3): ")
+
+            if choice == '1':
+                self.folder_analysis.run()
+            elif choice == '2':
+                self.image_analysis.run()
+            elif choice == '3':
+                break
+            else:
+                print('Select a number from the list.')
 
 if __name__ == "__main__":
-    main()
+    MainProgram().run()
